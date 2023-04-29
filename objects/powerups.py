b7f5d372018907b7f2ce8f4a31acedd23707b61f
Lumiobyte <evanpartridge0@gmail.com>
2023-04-30 00:24:04 +1000

    Revert pineapple speed increase change

--- objects/powerups.py
+++ objects/powerups.py
@@ -15,7 +15,7 @@ class Pineapple():
         self.sprite = pygame.transform.scale(self.sprite, (self.sprite.get_width() / 6, self.sprite.get_height() / 6))
         self.col_rect = pygame.Rect(*self.position.tuple(), self.sprite.get_width(), self.sprite.get_height())
 
-        self.speed_increase = 1 # 2
+        self.speed_increase = 2 # 2
 
         self.collected = False # has been picked up by player
         self.effected = False # its effect has been applied to the player? 
